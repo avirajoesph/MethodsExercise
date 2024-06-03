da9e1f04885b6aaaa61e6ba9121c7fae68919bcf
Derrick <Avirajoesph@yahoo.com>
2024-06-03 11:57:04 -0500

    saved story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,71 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello, World!");
+            CreateStory();
+            Add(9, 7);
+            Subtract (9, 7);
+            Divide (10, 2);
+            Multiply(2, 5);
+        
+        }
+        public static String CreateStory() 
+        {
+            Console.WriteLine("what is your name");
+            string name = Console.ReadLine ();
+
+            Console.WriteLine("what is your favorite color");
+            string favoriteColor = Console.ReadLine();
+
+            Console.WriteLine("what is your favorite animail");
+            string animail  = Console.ReadLine();
+
+            Console.WriteLine("what is your favorite band");
+            string band = Console.ReadLine();
+
+            string story = $"{name} loves the Color {favoriteColor} there favorite animail is {animail} there favorite band  is {band}";
+
+            Console.WriteLine(story);
+            return story;
+        
+        }
+        public static int Add(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+            return sum;
+        
+        
+        
+        }
+        public static int Subtract (int num1, int num2)
+        {
+            int diffrence = num1 - num2;
+            Console.WriteLine(diffrence);
+            return diffrence;
+
+
+
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            int mult = num1 * num2;
+            Console.WriteLine(mult);
+            return mult;
+
+
+
+        }
+        public static int Divide (int num1, int num2)
+        {
+            int divide = num1 / num2;
+            Console.WriteLine(divide);
+            return divide;
+
+
+
         }
     }
 }
